3fcdf6d9d08bc2a58d6a779030d36ab703819139
hyeongju you <61458960+hyeongju12@users.noreply.github.com>
2021-12-15 20:03:36 +0900

    숫자 관련 메서드 및 연습문제 푸는중

--- 자료구조/숫자/chapter1.py
+++ 자료구조/숫자/chapter1.py
@@ -16,7 +16,7 @@ val = n.bit_length()
 print(val)
 
 
-# 10진법으로 변환 
+# int(정수)
 s = '11'
 val = int(s)
 print(val)
@@ -30,3 +30,34 @@ print(val)
 # base가 2면 s를 구성하는 숫자는 0, 1이여야 한다.
 val3 = int(s, 2)
 print(val3)
+
+# float(부동소수점)
+# 부동소수점은 이준수 분수로 표현되기 떄문에 함부로 비교하거나 빼면 안된다.
+# 2진법으로 표현하기 어려운 숫자.(0.1)
+
+print(bool(0.2 * 3 == 0.6))
+print(bool(1.2 - 0.2 == 1.0))
+print(bool(1.2 - 0.1 == 1.1))
+print(0.1 * 0.1 == 0.01)
+
+# 정수와 부동소수점 관련 메서드
+
+# divmod(x, y) x를 y로 나눌때 (몫, 나머지)를 반환한다.
+result = divmod(45, 6)
+print(result)
+
+# round(x, n) x를 n<0 경우 n자리수 만큼 반올림 한 값 반환, n>0 n자리로 반올림한 값을 반환
+result1 = round(100.12, -2)
+result2 = round(100.12, 2)
+
+print(result1)
+print(result2)
+
+# as_integer_ratio() 부동소수점을 분수로 표현
+result3 = 0.1.as_integer_ratio()
+print(result3)
+
+# 2진수, 8진수, 16진수
+print(bin(999))
+print(oct(999))
+print(hex(999))
---
+++ 자료구조/숫자/chapter1_practices.py
@@ -0,0 +1,29 @@
+# Chapter1 연습문제 5개 
+
+# 1번 진법변환
+# 내용 : 다른 진법의 숫자를 10진수로 변환하는 함수를 직접 만들어보자.
+
+def convert_to_decimal(number, base):
+    multiple, result = 1, 0
+    while number > 0:
+        result += number % 10 * multiple
+        multiple *= base
+        number = number // 10
+    return result
+
+def test_convert_to_decimal():
+    number, base = 1001, 2
+    assert(convert_to_decimal(number, base) == 9)
+    print("테스트 통과")
+
+
+if __name__ == "__main__":
+    test_convert_to_decimal()
+
+# 최대공약수
+
+# random 모듈
+
+# 피보나치 수열
+
+# 소수 판단ㅎㅅ
