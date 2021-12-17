cb1504921dfdec60e39894c8a2e86ce663bf501d
hyeongju you <61458960+hyeongju12@users.noreply.github.com>
2021-12-17 18:27:23 +0900

    3자리 수의 중앙값 구하기

--- 알고리즘/Chapter1-01.py
+++ 알고리즘/Chapter1-01.py
@@ -1,11 +1,20 @@
-print("세 정수 중 최대값을 구하시오 :")
-a = int(input("정수를 입력하세요:"))
-b = int(input("정수를 입력하세요:"))
-c = int(input("정수를 입력하세요:"))
+# print("세 정수 중 최대값을 구하시오 :")
+# a = int(input("정수를 입력하세요:"))
+# b = int(input("정수를 입력하세요:"))
+# c = int(input("정수를 입력하세요:"))
 
-maximum = a
+# maximum = a
 
-if maximum < b: maximum = b
-if maximum < c: maximum = c
+# if maximum < b: maximum = b
+# if maximum < c: maximum = c
 
-print(f'최대값은 {maximum}입니다.')
+# print(f'최대값은 {maximum}입니다.')
+
+def maximum(a, b, c):
+    maximum = a
+    if maximum < b : maximum = b
+    if maximum < c : maximum = c
+    return maximum
+
+
+print(f'{maximum(4,5,6)}')
---
+++ 알고리즘/Chapter1-02.py
@@ -0,0 +1,18 @@
+def median(a, b, c):
+    if a >= b:
+        if b >= c:
+            return b
+        elif a <= c:
+            return a
+        else:
+            return c
+    elif a > c:
+        return a
+    elif b > c:
+        return c
+    else:
+        return b
+    
+
+
+print(f'{median(4,5,6)}')
