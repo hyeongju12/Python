dc86a0e188972b7bad55719ed273513d9ccee5d3
hyeongju you <61458960+hyeongju12@users.noreply.github.com>
2022-01-05 17:31:12 +0900

    데코레이터

--- 중급강의/2022-01-03-Chapter5-4.py
+++ 중급강의/2022-01-03-Chapter5-4.py
@@ -1,67 +1,52 @@
-# Chapter05-02
-# 일급 함수(일급 객체)
-# 클로저 기초
-# 외부에서 호출된 함수의 변수값, 상태(레퍼런스) 복사 후 저장 -> 차 후 접근 가능
+# Chater5-4
+# 데코레이터
+# 데코레이터(Decorator)
 
-# closure 사용
-def closure_ex1():
-    # Free variable : 사용하려는 함수 밖에 존재하는 변수
-    # 클로져 영역
-    series = []
-    def averager(v):
-        series.append(v)
-        print('inner >> {} / {}'.format(series, len(series)))
-        return sum(series) / len(series)
-    return averager
+# 장점
+# 1. 중복 제거, 코드 간결화, 공통 함수 작성
+# 2. 로깅, 프레임워크, 유효성 체크 -> 공통 기능
+# 3. 조합해서 사용하기 용이하다.
 
-avg_closure1 = closure_ex1()
+# 단점
+# 1. 가독성 저하
+# 2. 특정 기능에 한정된 함수는 단일 함수로 작성하는 것이 유리
+# 3. 디버깅 불편
 
-print(avg_closure1(10))
-print(avg_closure1(30))
-print(avg_closure1(50))
+# 데코레이터 실습
 
+import time
+
+def perf_clock(func):
+    def perf_clocked(*args):
+        st = time.perf_counter()
+        result = func(*args)
+        et = time.perf_counter() - st
+        name = func.__name__
+        arg_str = ', '.join(repr(arg) for arg in args)
+        print('[%0.5fs] %s(%s) -> %r' % (et, name, arg_str, result))
+        return result
+    return perf_clocked
+
+@perf_clock
+def time_func(seconds):
+    time.sleep(seconds)
+@perf_clock
+def sum_func(*numbers):
+    return sum(numbers)
+
+# 데코레이터 미사용
+none_deco1 = perf_clock(time_func)
+none_deco2 = perf_clock(sum_func)
+
+print(none_deco1, none_deco1.__code__.co_freevars)
+print(none_deco2, none_deco2.__code__.co_freevars)
+
+print('-' * 40, "Called None Decorator -> time_func")
 print()
+none_deco1(1.5)
+print('-' * 40, "Called None Decorator -> sum_func")
 print()
+none_deco2(100, 200, 300, 400, 500)
 
-# function inspection
-print(dir(avg_closure1))
-print()
-print(dir(avg_closure1.__code__))
-print()
-print(avg_closure1.__code__.co_freevars)
-print(avg_closure1.__closure__[0].cell_contents)
-
-# 잘못된 클로져 사용법
-def closure_ex2():
-    # free variable
-    cnt = 0
-    total = 0
-
-    def averager(v):
-        cnt += 1
-        total += v
-        return total / cnt
-
-    return averager
-
-# avg_closure2 = closure_ex2()
-# print(avg_closure2(10))
-
-def closure_ex3():
-    # free variable
-    cnt = 0
-    total = 0
-
-    def averager(v):
-        nonlocal cnt, total
-        cnt += 1
-        total += v
-        return total / cnt
-
-    return averager
-
-avg_closure3 = closure_ex3()
-
-print(avg_closure3(10))
-print(avg_closure3(25))
-print(avg_closure3(35))
+time_func(1.5)
+sum_func(100, 200, 300, 400, 500)
