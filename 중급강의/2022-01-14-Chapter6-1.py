b2cda395e353ab675680ad86b2aa65a49248f394
hyeongju you <61458960+hyeongju12@users.noreply.github.com>
2022-01-14 20:31:42 +0900

    __next__를 제너레이터 패턴으로

--- 중급강의/2022-01-14-Chapter6-1.py
+++ 중급강의/2022-01-14-Chapter6-1.py
@@ -33,3 +33,71 @@ import collections
 # print(dir(t))
 print(hasattr(t, '__iter__'))
 print(isinstance(t, abc.Iterable))
+
+print()
+print()
+
+# __next__()
+class WordSplitter:
+    def __init__(self, text):
+        self._idx = 0
+        self._text = text.split(' ')
+
+    def __next__(self):
+        print('Called __next__')
+        try:
+            word = self._text[self._idx]
+        except IndexError:
+            raise StopIteration('Stopped Iteration.')
+        self._idx += 1
+        return word
+
+    def __repr__(self):
+        return 'WordSplit(%s)' % (self._text)
+
+wi = WordSplitter('Do today what you could do tommorrow')
+
+print(wi)
+print(next(wi))
+print(next(wi))
+print(next(wi))
+print(next(wi))
+print(next(wi))
+print(next(wi))
+print(next(wi))
+# print(next(wi))
+
+print()
+print()
+
+# Generator 패턴
+# 1. 지능형 리스트, 딕셔너리, 집합 -> 데이터 양이 엄청 증가한 경우 메모리 사용량 증가 -> 이러한 상황을 피하고자 제너레이터 사용 권장
+# 2. 단위 실행 가능한 코루틴(Coroutine)구현과 연동
+# 3. 작은 메모리 조각 사용
+
+class WordSplitGenerator:
+    def __init__(self, text):
+        self._text = text.split(' ')
+
+    def __iter__(self):
+        for word in self._text:
+            yield word # 제너레이터 / 리턴이 없어도됨..
+        return
+
+    def __repr__(self):
+        return 'WordSplitGenerator(%s)' % (self._text)
+
+
+wg = WordSplitGenerator('Do today what you could do tommorrow')
+
+wt = iter(wg)
+
+print(wt, wg)
+print(next(wt))
+print(next(wt))
+print(next(wt))
+print(next(wt))
+print(next(wt))
+print(next(wt))
+print(next(wt))
+print(next(wt))
