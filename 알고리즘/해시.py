400967f47a5cb74ade1b788329cd8aab74b784f5
hyeongju you <61458960+hyeongju12@users.noreply.github.com>
2022-03-09 14:46:13 +0900

    해시

--- 알고리즘/해시.py
+++ 알고리즘/해시.py
@@ -1,5 +1,16 @@
+from enum import Enum
 from typing import Any
 import hashlib
+
+Menu = Enum('Menu', ['추가', '삭제', '검색', '덤프', '종료'])
+
+def select_menu() -> Menu:
+    s = [f'({m.value}){m.name}' for m in Menu]
+    while True:
+        print(*s, sep = '  ', end='')
+        n = int(input(' :'))
+        if 1 <= n <= len(Menu):
+            return Menu(n)
 
 class Node: # 해시를 구성하는 노드
     def __init__(self, key: Any, value: Any, next: None) -> None:
@@ -66,3 +77,33 @@ class ChainedHash:
                 print(f' -> {p.key} ({p.value})', end='')
                 p = p.next
             print()
+
+hash = ChainedHash(13)
+
+while True:
+    menu = select_menu()
+    if menu == Menu.추가 :
+        key = int(input('추가할 키를 입력하세요 : '))
+        val = input('추가할 값을 입력하세요')
+        if not hash.add(key, val):
+            print('추가 실패!')
+
+    elif menu == Menu.삭제:
+        key = int(input('삭제할 키를 입력하세요 : '))
+        if not hash.remove(key):
+            print('삭제 실패!')
+
+    elif menu == Menu.검색 :
+        key = int(input('검색할 키를 입력하세요 : '))
+        t = hash.search(key)
+        if t is not None:
+            print(f'검색한 키가 갖는 값은 {t} 입니다.')
+        else:
+            print('검색할 데이터가 없습니다.')
+    elif menu == Menu.덤프:
+        hash.dump()
+
+    else:
+        break
+
+
